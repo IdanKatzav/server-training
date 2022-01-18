ed18b13eb43438f428399fb45a4d63eb035c294d
IdanKatzav <idan.katzav@gmail.com>
2022-01-19 01:29:49 +0200

    feat: finish ex 1

---
+++ src/server/server.ts
@@ -0,0 +1,10 @@
+import {server} from './controller';
+
+
+const hostname = '127.0.0.1';
+const port = 4321;
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
